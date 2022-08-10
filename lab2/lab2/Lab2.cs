87734816d2ef929d4b91f30e9294bfb7f5616dd8
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 12:56:24 +0300

    Add files via upload

---
+++ lab2/lab2/Lab2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            const float e=0.001f;
+            double term=0, sum=0;
+            int factorial=1;
+
+            do
+            {
+                for (int j = 1; j>e; j++)
+                {
+                    factorial *= j;
+                    if (j % 2 == 1)
+                    {
+                        term = -(Math.Pow(1, j) / factorial);
+                        sum += term;
+                    }
+                    else 
+                    {
+                        term = (Math.Pow(2, j) / factorial);
+                        sum += term;
+                    }
+                   
+                }
+            } while (term > e);
+
+                Console.WriteLine($"sum={sum}");
+            Console.ReadKey();
+        }
+    }
+}
